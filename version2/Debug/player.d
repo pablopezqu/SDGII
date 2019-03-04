2955cf8f3be43d4aa3b080846f5075f3fd765c47
isardinero <47307503+isardinero@users.noreply.github.com>
2019-03-04 19:56:10 +0100

    Add files via upload
    Versión 2.0 (pendiente de verificación)

---
+++ version2/Debug/player.d
@@ -0,0 +1,16 @@
+player.o: ../player.c ../player.h \
+ D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/wiringPi.h \
+ ../piTankGoLib.h ../fsm.h ../tmr.h \
+ D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/softTone.h
+
+../player.h:
+
+D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/wiringPi.h:
+
+../piTankGoLib.h:
+
+../fsm.h:
+
+../tmr.h:
+
+D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/softTone.h:
---
+++ version2/Debug/torreta.d
@@ -0,0 +1,13 @@
+torreta.o: ../torreta.c ../torreta.h \
+ D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/wiringPi.h \
+ ../piTankGoLib.h ../fsm.h ../tmr.h
+
+../torreta.h:
+
+D:\Uni\TERCERO\SDG2\SysGCC\Raspberry\include\wiringPi/wiringPi.h:
+
+../piTankGoLib.h:
+
+../fsm.h:
+
+../tmr.h:
